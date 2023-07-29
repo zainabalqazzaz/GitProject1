da1f438a6f660713b7a3c369b4e0e6d11f974061
zaina <zaina@Yazen-HP-Labtop.fios-router.home>
2023-07-29 10:34:20 -0400

    implementted new method

--- src/day2/DemDay2.java
+++ src/day2/DemDay2.java
@@ -3,5 +3,15 @@ package day2;
 public class DemDay2 {
 public static void main(String[] args) {
 	System.out.println("test");
+	
+	
+}
+
+//add method
+public static void add(int a,int b) {
+	System.out.println(a+b);
+}
+public static void subtruct(int a,int b) {
+	System.out.println(a-b);
 }
 }
