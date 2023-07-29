1d40174b8ecd1fc98c2fd966cb55de26e247128e
zainabalqazzaz <140640712+zainabalqazzaz@users.noreply.github.com>
2023-07-29 11:07:40 -0400

    Update DemoDay3.java

--- src/day3/DemoDay3.java
+++ src/day3/DemoDay3.java
@@ -3,5 +3,10 @@ package day3;
 public class DemoDay3 {
 public static void main(String[] args) {
 	System.out.println("zainab");
+
+	String country="USA";
+	String state="VA";
+	String city="fairfax";
+	
 }
 }
