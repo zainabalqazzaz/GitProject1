4c9596bf5186265c7f43e1ac298d1b915a6997e2
zainabalqazzaz <140640712+zainabalqazzaz@users.noreply.github.com>
2023-07-29 13:40:37 -0400

    Update DemoDay9.java

--- src/day9/DemoDay9.java
+++ src/day9/DemoDay9.java
@@ -4,4 +4,7 @@ public class DemoDay9 {
 public static void main(String[] args) {
 	System.out.println("is to cold");
 }
+	public static void getInfo(){
+		System.out.println("my name is zainab");
+	}
 }
